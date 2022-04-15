cf6aa498983cf089a854e3c26219a175b98235d9
victorpeigo <victorperigo@hotmail.com>
2022-04-15 03:53:37 -0300

    refatorando e opcao default

--- main.go
+++ main.go
@@ -10,8 +10,9 @@ func main() {
 	fmt.Println("2 - Exibir os logs")
 	fmt.Println("0 - Sair do programa")
 
-	var opcao int
-	fmt.Scanf("%d", &opcao)
+	opcao := 5
+	// variavel opcao recebe o valor digitado pelo usuario, sem expecificar o tipo do input, o proprio go ira inferir o tipo apartir da variavel opcao
+	fmt.Scan(&opcao)
 
 	switch opcao {
 	case 1: // Iniciar monitoramento
@@ -20,6 +21,8 @@ func main() {
 		fmt.Println("Exibindo os logs...")
 	case 0: // Sair do programa
 		fmt.Println("Saindo do programa...")
+	default: // Caso o usuario digite uma opcao invalida
+		fmt.Println("Opção invalida")
 	}
 
 }
