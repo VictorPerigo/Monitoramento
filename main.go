801b38d102a7d0cd171b5f53dd0a395175fb69c4
victorpeigo <victorperigo@hotmail.com>
2022-04-15 05:40:14 -0300

    menu urls v1

--- main.go
+++ main.go
@@ -10,14 +10,9 @@ func main() {
 	exibirMenu()
 }
 
-func exibirMenu() {
-	for {
-		fmt.Println("\nOque deseja fazer?")
-		fmt.Println("1 - Iniciar monitoramento")
-		fmt.Println("2 - Exibir os logs")
-		fmt.Println("0 - Sair do programa:")
-		menuFuncionamento()
-	}
+var urls []string = []string{
+	"https://www.google.com.br/notfound",
+	"https://www.youtube.com",
 }
 
 func menuFuncionamento() {
@@ -26,6 +21,8 @@ func menuFuncionamento() {
 		monitoramento()
 	case 2: // Exibir os logs
 		fmt.Println("Exibindo os logs...")
+	case 3: // Urls
+		menuUrls()
 	case 0: // Sair do programa
 		fmt.Println("Saindo do programa...")
 		os.Exit(0)
@@ -45,10 +42,6 @@ func inputOpcao() int {
 
 func monitoramento() {
 	fmt.Print("\033[H\033[2J")
-	urls := []string{
-		"https://www.google.com.br/jonas",
-		"https://www.youtube.com",
-	}
 
 	for _, url := range urls {
 		response, err := http.Get(url)
@@ -64,4 +57,73 @@ func monitoramento() {
 		}
 	}
 
+}
+
+func menuUrls() {
+	exibirUrls()
+
+	exibirMenuUrl()
+
+	switch inputOpcao() {
+	case 1: // Adicionar url
+		adicionarUrl()
+	case 2: // Remover url
+		removerUrl()
+	case 0: // Sair do programa
+		fmt.Println("Saindo do programa...")
+		fmt.Println("\033[H\033[2J")
+	default: // Caso o usuario digite uma opcao invalida
+		fmt.Println("Opção invalida")
+	}
+}
+
+func adicionarUrl() {
+	exibirUrls()
+	fmt.Println("Digite a url:")
+	var url string
+	fmt.Scan(&url)
+	urls = append(urls, url)
+	fmt.Println("Url adicionada com sucesso!")
+}
+
+func removerUrl() {
+	exibirUrls()
+	fmt.Println("\nDigite o index da a url que deseja remover:")
+	var indexEscolhido int
+	fmt.Scan(&indexEscolhido)
+	for index := range urls {
+		if index == indexEscolhido {
+			urls = append(urls[:index], urls[index+1:]...)
+			fmt.Println("Url removida com sucesso!")
+			break
+		}
+	}
+}
+
+// exibicoes:
+
+func exibirUrls() {
+	fmt.Print("\033[H\033[2J")
+	fmt.Println("Urls:")
+	for index, url := range urls {
+		fmt.Println("index:", index, ", url:", url)
+	}
+}
+
+func exibirMenuUrl() {
+	fmt.Println("\nOque deseja fazer?")
+	fmt.Println("1 - Adicionar url")
+	fmt.Println("2 - Remover url")
+	fmt.Println("0 - Voltar ao menu:")
+}
+
+func exibirMenu() {
+	for {
+		fmt.Println("\nOque deseja fazer?")
+		fmt.Println("1 - Iniciar monitoramento")
+		fmt.Println("2 - Exibir os logs")
+		fmt.Println("3 - Urls")
+		fmt.Println("0 - Sair do programa:")
+		menuFuncionamento()
+	}
 }
