4698fbe872fcd2f18a6a28ddae169f14a42b847a
victorpeigo <victorperigo@hotmail.com>
2022-04-15 04:09:27 -0300

    separando em funcoes

--- main.go
+++ main.go
@@ -2,27 +2,40 @@ package main
 
 import (
 	"fmt"
+	"os"
 )
 
 func main() {
-	fmt.Println("Olá, que deseja fazer?")
-	fmt.Println("1 - Iniciar monitoramento")
-	fmt.Println("2 - Exibir os logs")
-	fmt.Println("0 - Sair do programa")
+	exibirMenu()
+	menuFuncionamento()
+}
 
+func inputOpcao() int {
 	opcao := 5
 	// variavel opcao recebe o valor digitado pelo usuario, sem expecificar o tipo do input, o proprio go ira inferir o tipo apartir da variavel opcao
 	fmt.Scan(&opcao)
 
-	switch opcao {
+	return opcao
+}
+
+func exibirMenu() {
+	fmt.Println("Olá, que deseja fazer?")
+	fmt.Println("1 - Iniciar monitoramento")
+	fmt.Println("2 - Exibir os logs")
+	fmt.Println("0 - Sair do programa:")
+}
+
+func menuFuncionamento() {
+	switch inputOpcao() {
 	case 1: // Iniciar monitoramento
 		fmt.Println("Monitorando...")
 	case 2: // Exibir os logs
 		fmt.Println("Exibindo os logs...")
 	case 0: // Sair do programa
 		fmt.Println("Saindo do programa...")
+		os.Exit(0)
 	default: // Caso o usuario digite uma opcao invalida
 		fmt.Println("Opção invalida")
+		os.Exit(-1)
 	}
-
 }
